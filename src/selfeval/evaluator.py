6f7792d1b941ebba42077e58c8f196360f42f0a6
Natyren <bredis.georgy@yandex.ru>
2023-12-24 03:27:27 +0300

    add simple evalutaor base, little refactor on architecture

---
+++ src/selfeval/evaluator.py
@@ -0,0 +1,12 @@
+import torch
+
+
+class Evaluator:
+    def __init__(self, scheduler, model, timesteps):
+        pass
+
+    def evaluate_on_step(self, timestep):
+        pass
+
+    def evaluate(self, image: torch.Tensor):
+        pass
